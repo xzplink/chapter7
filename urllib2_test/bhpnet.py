e0d21d846a9a87fb39790d8de08b84491d6e65dc
vinglong <vinglong@163.com>
2016-09-20 22:34:16 +0800

    update bhpnet.py

--- urllib2_test/bhpnet.py
+++ urllib2_test/bhpnet.py
@@ -36,6 +36,119 @@ def usage():
     print "echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135"
     sys.exit(0)
 
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # 连接到目标主机
+        client.connect((target,port))
+
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            # 现在等待数据回传
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+            print response
+
+            # 等待更多的输入
+            buffer = raw_input("")
+            buffer += "\n"
+
+            # 发送出去
+            client.send(buffer)
+    except:
+        print "[*] Exception! Exiting."
+        # 关闭连接
+        client.close()
+
+def server_loop():
+    global target
+
+    # 如果没有定义目标，那么我们监听所有接口
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target,port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        # 分析一个线程处理的客户端
+        client_thread = thread.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+
+def run_command(command):
+    # 换行
+    command = command.rstrip()
+    # 运行命令行并将输出返回
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+    # 将输出发送
+    return output
+
+def client_hanlder(client_socket):
+    global upload
+    global execute
+    global command
+
+    # 检测上传文件
+    if len(upload_destination):
+        # 读取所有的字符并写下目标
+        file_buffer = ""
+        # 持续读取数据直到没有符合的数据
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+        # 现在接收这些数据并将他们写出来
+        try:
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+
+            # 确认文件已经写出来
+            client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
+        except:
+            client_socket.send("Failed to save file to %s\r\n" % upload_destination)
+
+    # 检查命令执行
+    if len(execute):
+        # 运行命令
+        output = run_command(execute)
+        client_socket.send(output)
+
+    # 如果需要一个命令行shell，那么我们进入另一个循环
+    if command:
+        while True:
+            # 跳出一个窗口
+            client_socket.send("<BHP:#> ")
+            # 现在我们接收文件直到发现换行符(enter key)
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+                # 返还命令输出
+                response = run_command(cmd_buffer)
+                # 返回响应数据
+                client_socket.send(response)
+
 def main():
     global listen
     global port
